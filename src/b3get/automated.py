edc9e3b92028a91dbb27045687539e994808175b
Peter Steinbach <steinbach@scionics.de>
2019-03-11 17:01:44 +0100

    added automated functions to offer the functional pythonista API of this package

---
+++ src/b3get/automated.py
@@ -0,0 +1,33 @@
+import os
+from b3get.datasets import *
+
+def to_numpy(dataset_id = None, labels_match='foreground'):
+    """ function to download and convert dataset of ID <dataeset_id>
+    return value: tuple (size 2)
+    - item 0: images associated with this dataset
+    - item 1: labels selected according to <labels_match>
+    """
+
+    value = (None, None)
+    dsint = int(dataset_id)
+    if not dsint in [6,8,24,27]:
+        print('support for BBBC{0:03} planned, but not available yet'.format(dsint))
+        return value
+
+    ds = eval('ds_{0:03}()'.format(dsint))
+    imgs = ds.pull_images()
+    ximgs = ds.extract_images()
+
+    if len(ximgs) > 0:
+        ximgs = sorted(ximgs)
+        basepath = os.path.split(ximgs[0])[0]
+        value = (ds.to_numpy(basepath), None)
+
+    labs = ds.pull_gt(labels_match)
+    xlabs = ds.extract_gt()
+    if len(xlabs) > 0:
+        xlabs = sorted(xlabs)
+        basepath = os.path.split(xlabs[0])[0]
+        value = (value[0], ds.to_numpy(basepath))
+
+    return value
---
+++ tests/test_automated.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from b3get.utils import tmp_location
+
+from b3get import to_numpy
+import numpy as np
+
+
+def test_available():
+    assert dir(to_numpy)
+
+
+def test_wrong_ds():
+    assert to_numpy(9) == (None, None)
+
+
+def test_008():
+    tgt = os.path.join(tmp_location(),'BBBC008')
+    if os.path.exists(tgt):
+        shutil.rmtree(tgt)
+
+    imgs, labs = to_numpy(8)
+    assert len(imgs) > 0
+    assert len(imgs) == 24
+
+    assert isinstance(imgs[0], np.ndarray)
+    assert imgs[0].shape == (512, 512)
+    assert imgs[0].dtype == np.uint8
+
+    assert isinstance(labs[0], np.ndarray)
+    assert labs[0].shape == (512, 512)
+    assert imgs[0].dtype == np.uint8
+
+    shutil.rmtree(tgt)
