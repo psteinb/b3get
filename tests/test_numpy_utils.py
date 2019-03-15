70bfd37881bdef2ecd6a7bbfc105a001b3e97749
Peter Steinbach <steinbach@scionics.de>
2019-03-15 15:44:19 +0100

    fixed failing tests due to ordering of arbitrary file descriptors in numpy's npz format

--- tests/test_numpy_utils.py
+++ tests/test_numpy_utils.py
@@ -7,7 +7,10 @@ from b3get.utils import chunk_npz
 
 @pytest.fixture
 def list_of_ndarrays():
-    return [np.ones(shape=(256,256), dtype='uint32')*n for n in range(16)]  # 16*256KB
+    nplist = [np.ones(shape=(256, 256), dtype='uint32') for n in range(16)]  # 16*256KB
+    for idx in range(len(nplist)):
+        nplist[idx].fill(int(idx))
+    return nplist
 
 
 def test_fixture_values(list_of_ndarrays):
@@ -37,7 +40,7 @@ def test_np_savez_reopen(list_of_ndarrays):
 
     npt = np.load(tmpf)
     assert len(npt.files) == 16
-    fnames = sorted(npt.files)
+    fnames = sorted(npt.files, key=lambda x: int(x.split('_')[-1]))
     assert 'arr_0' in fnames
     assert 'arr_15' in fnames
     front = npt['arr_0']
@@ -52,6 +55,7 @@ def test_in_chunks(list_of_ndarrays):
     files = chunk_npz(list_of_ndarrays, tmpf, .5)
     assert len(files) > 0
     assert len(files) == 8
+
     front_size = os.stat(files[0]).st_size
     back_size = os.stat(files[-1]).st_size
 
@@ -64,20 +68,24 @@ def test_in_chunks(list_of_ndarrays):
     front = np.load(files[0])
     back = np.load(files[-1])
 
-    assert len(front.files) > 0
-    assert len(back.files) > 0
+    frontf = sorted(front.files, key=lambda x: int(x.split('_')[-1]))
+    backf = sorted(back.files, key=lambda x: int(x.split('_')[-1]))
 
-    assert front[front.files[0]].nbytes > 0
-    assert back[back.files[0]].nbytes > 0
+    assert len(frontf) > 0
+    assert len(backf) > 0
 
-    assert front[front.files[0]].shape == (256,256)
-    assert back[back.files[0]].shape == (256,256)
 
-    assert front[front.files[0]].shape == list_of_ndarrays[0].shape
-    assert np.all(front[front.files[0]] == list_of_ndarrays[0])
+    assert front[frontf[0]].nbytes > 0
+    assert back[backf[0]].nbytes > 0
 
-    assert back[back.files[-1]].shape == list_of_ndarrays[-1].shape
-    assert np.all(back[back.files[-1]] == list_of_ndarrays[-1])
+    assert front[frontf[0]].shape == (256,256)
+    assert back[backf[0]].shape == (256,256)
+
+    assert front[frontf[0]].shape == list_of_ndarrays[0].shape
+    assert np.all(front[frontf[0]] == list_of_ndarrays[0])
+
+    assert back[backf[-1]].shape == list_of_ndarrays[-1].shape
+    assert np.all(back[backf[-1]] == list_of_ndarrays[-1])
     [os.remove(f) for f in files]
 
 
@@ -97,8 +105,12 @@ def test_in_chunks_too_large(list_of_ndarrays):
 def test_with_quarter_gb():
 
     list_of_ndarrays = [np.ones(shape=(256, 256, 256), dtype='uint32') for i in range(16)]
-    for idx, npa in enumerate(list_of_ndarrays):
-        npa = npa*idx
+    for idx in range(len(list_of_ndarrays)):
+        list_of_ndarrays[idx].fill(int(idx))
+
+    assert list_of_ndarrays[0][0,0,0] == 0
+    assert list_of_ndarrays[1][0,0,0] == 1
+    assert list_of_ndarrays[2][0,0,0] == 2
 
     tmpf = tempfile.mktemp()
     files = chunk_npz(list_of_ndarrays, tmpf, 128)
@@ -113,19 +125,22 @@ def test_with_quarter_gb():
     front = np.load(files[0])
     back = np.load(files[-1])
 
-    assert len(front.files) > 0
-    assert len(back.files) > 0
-    assert len(back.files) == len(front.files)
+    frontf = sorted(front.files, key=lambda x: int(x.split('_')[-1]))
+    backf = sorted(back.files, key=lambda x: int(x.split('_')[-1]))
 
-    assert front[front.files[0]].nbytes > 0
-    assert back[back.files[0]].nbytes > 0
+    assert len(frontf) > 0
+    assert len(backf) > 0
+    assert len(back) == len(frontf)
 
-    assert front[front.files[0]].shape == (256, 256, 256)
-    assert back[back.files[0]].shape == (256, 256, 256)
+    assert front[frontf[0]].nbytes > 0
+    assert back[backf[0]].nbytes > 0
 
-    assert front[front.files[0]].shape == list_of_ndarrays[0].shape
-    assert np.all(front[front.files[0]] == list_of_ndarrays[0])
+    assert front[frontf[0]].shape == (256, 256, 256)
+    assert back[backf[0]].shape == (256, 256, 256)
 
-    assert back[back.files[-1]].shape == list_of_ndarrays[-1].shape
-    assert np.all(back[back.files[-1]] == list_of_ndarrays[-1])
+    assert front[frontf[0]].shape == list_of_ndarrays[0].shape
+    assert np.all(front[frontf[0]] == list_of_ndarrays[0])
+
+    assert back[backf[-1]].shape == list_of_ndarrays[-1].shape
+    assert np.all(back[backf[-1]] == list_of_ndarrays[-1])
     [os.remove(f) for f in files]
